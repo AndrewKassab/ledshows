7d16e50998adc2c31921949dc733f13fada37c0e
andrewkassab <andrewkassab@live.com>
2020-03-13 19:00:51 -0700

    updated cycle to have the speedup phase

--- sequences/sequence_cycle/sequence_cycle.ino
+++ sequences/sequence_cycle/sequence_cycle.ino
@@ -16,62 +16,76 @@ void loop() {
   square_color_trace(leds_list, 1, 14, CRGB::Green);
   square_color_trace(leds_list, 1, 14, CRGB::Blue);
   square_color_trace(leds_list, 1, 14, CRGB::ForestGreen);
+  square_color_trace(leds_list, 1, 14, CRGB::Fuchsia);
   square_color_trace(leds_list, 1, 14, CRGB::Red);
 
   color_trace_bridges_to_center(leds_list, 14, CRGB::Red);
   color_trace_outwards_from_center(leds_list, 14, CRGB::Cyan);
+  square_color_trace(leds_list, 1, 14, CRGB::Blue);
+  color_trace_bridges_to_center(leds_list, 14, CRGB::Blue);
 
   allLights.fadeToBlack(15);
 
-  for ( int i = 0; i < 3; i++ ){
-    square_side_cycle(leds_list, 550, CRGB::ForestGreen, CRGB::DeepPink, CRGB::ForestGreen, CRGB::DeepPink);
-    delay(550);
-    square_side_cycle(leds_list, 550, CRGB::Gold, CRGB::SkyBlue, CRGB::Gold, CRGB::SkyBlue);
-    delay(550);
+  for ( int i = 0; i < 4; i++ ){
+    square_side_cycle(leds_list, 500, CRGB::ForestGreen, CRGB::DeepPink, CRGB::ForestGreen, CRGB::DeepPink);
+    delay(500);
+    square_side_cycle(leds_list, 500, CRGB::Gold, CRGB::SkyBlue, CRGB::Gold, CRGB::SkyBlue);
+    delay(500);
   }
 
   allLights.fadeToBlack(5);
 
   for ( int i = 0; i <= 2; i++ ){
-    up_down_trace(leds_list, 4, 18, CRGB::DeepPink, CRGB::Cyan);
-    up_down_trace(leds_list, 4, 18, CRGB::DeepPink, CRGB::Cyan);
     allLights.turnOff();
-    left_right_trace(leds_list, 4, 18, CRGB::ForestGreen, CRGB::Purple);
-    left_right_trace(leds_list, 4, 18, CRGB::ForestGreen, CRGB::Purple);
+    up_down_trace(leds_list, 5, 16, CRGB::DeepPink, CRGB::Cyan);
+    up_down_trace(leds_list, 5, 16, CRGB::DeepPink, CRGB::Cyan);
     allLights.turnOff();
+    left_right_trace(leds_list, 5, 16, CRGB::ForestGreen, CRGB::Purple);
+    left_right_trace(leds_list, 5, 16, CRGB::ForestGreen, CRGB::Purple);
   }
-
-  blink_all_lights(leds_list, CRGB::White, 250);
-  blink_all_lights(leds_list, CRGB::Blue, 250);
-  blink_all_lights(leds_list, CRGB::Gold, 250);
 
   allLights.fadeToBlack(15);
 
+  blink_all_lights(leds_list, CRGB::White, 250);
+  delay(250);
+  blink_all_lights(leds_list, CRGB::Blue, 250);
+  delay(250);
+  blink_all_lights(leds_list, CRGB::Gold, 250);
+  delay(250);
+  blink_all_lights(leds_list, CRGB::DeepPink, 250);
+
+  allLights.fadeToBlack(20);
+
   for ( int i = 0; i < 2; i++ ){
-    corners_cycle_towards_center(leds_list, 550, CRGB::ForestGreen, CRGB::DeepPink);
-    allLights.fadeToBlack(10);
-    corners_cycle_towards_center(leds_list, 550, CRGB::Gold, CRGB::Cyan);
-    allLights.fadeToBlack(10);
+    corners_cycle_towards_center(leds_list, 500, CRGB::ForestGreen, CRGB::DeepPink);
+    allLights.fadeToBlack(13);
+    corners_cycle_towards_center(leds_list, 500, CRGB::Gold, CRGB::Cyan);
+    allLights.fadeToBlack(13);
   }
 
   squares_left_right_dash_up_down(leds_list, 12, CRGB::Red, CRGB::Blue);
   squares_left_right_dash_up_down(leds_list, 12, CRGB::DeepPink, CRGB::Cyan);
   squares_left_right_dash_up_down(leds_list, 12, CRGB::Gold, CRGB::ForestGreen);
+  squares_left_right_dash_up_down(leds_list, 12, CRGB::Red, CRGB::Blue);
 
-  delay(150);
+  delay(450);
 
   for ( int i = 0; i <= 2; i++ ){
-    sides_to_middle(leds_list, 600, CRGB::Blue, CRGB::Gold, CRGB::Cyan);
-    delay(600);
-    top_to_bottom(leds_list, 600, CRGB::Red, CRGB::Green);
-    delay(600);
-    sides_to_middle(leds_list, 600, CRGB::Blue, CRGB::Gold, CRGB::Cyan);
-    delay(600);
-    top_to_bottom(leds_list, 600, CRGB::Red, CRGB::Green);
-    delay(600);
+    sides_to_middle(leds_list, 450, CRGB::Blue, CRGB::Gold, CRGB::Cyan);
+    delay(525);
+    top_to_bottom(leds_list, 450, CRGB::Red, CRGB::Green);
+    delay(525);
+    sides_to_middle(leds_list, 450, CRGB::Blue, CRGB::Gold, CRGB::Cyan);
+    delay(525);
+    top_to_bottom(leds_list, 450, CRGB::Red, CRGB::Green);
+    delay(525);
   }
 
-  allLights.fadeToBlack(10);
+  allLights.fadeToBlack(20);
+
+  square_cycle_speed_up_rainbow(leds_list, 200, 15, 35);
+
+  allLights.fadeToBlack(20);
 
   squares_top_bottom_dash_left_right(leds_list, 12, CRGB::Blue, CRGB::Gold);
   squares_top_bottom_dash_left_right(leds_list, 12, CRGB::Blue, CRGB::Gold);
--- songs/something_comforting/something_comforting.ino
+++ songs/something_comforting/something_comforting.ino
@@ -13,27 +13,29 @@ void loop(){
   FastLED.clear();
   FastLED.show();
 
+  
   // 820 for half speed
   // 420 normal
   delay(420);
 
-  blink_all_lights(leds,CRGB::White, 200);
+  blink_all_lights_with_ending(leds,CRGB::White, 200);
   delay(200);
-  blink_all_lights(leds, CRGB::Blue, 200);
-  delay(200);
-  FastLED.clear();
-  FastLED.show();
-  blink_all_lights(leds, CRGB::Gold, 200);
-  delay(200);
-  blink_all_lights(leds, CRGB::Cyan, 200);
+  blink_all_lights_with_ending(leds, CRGB::Blue, 200);
   delay(200);
   FastLED.clear();
   FastLED.show();
-  blink_all_lights(leds, CRGB::DeepPink, 200);
+  blink_all_lights_with_ending(leds, CRGB::Gold, 200);
   delay(200);
-  blink_all_lights(leds, CRGB::ForestGreen, 200);
+  blink_all_lights_with_ending(leds, CRGB::Cyan, 200);
+  delay(200);
+  FastLED.clear();
+  FastLED.show();
+  blink_all_lights_with_ending(leds, CRGB::DeepPink, 200);
+  delay(200);
+  blink_all_lights_with_ending(leds, CRGB::ForestGreen, 200);
   delay(225);
   allLights.turnOff();
+  delay(10);
 
   corners_cycle_towards_center(leds, 410, CRGB::SkyBlue, CRGB::ForestGreen);
   allLights.fadeToBlack(1);
@@ -44,93 +46,119 @@ void loop(){
   corners_cycle_towards_center(leds, 410, CRGB::SkyBlue, CRGB::ForestGreen);
   allLights.fadeToBlack(1);
 
-  alternate_squares_sides_and_tops(leds, 410, CRGB::SkyBlue, CRGB::ForestGreen);
-  allLights.turnOff();
-  delay(410);
+  // boom boom clap claps
+  for ( int i = 0; i <= 14; i++){
+    alternate_squares_sides_and_tops(leds, 410, CRGB::SkyBlue, CRGB::ForestGreen);
+    delay(407);
     allLights.turnOff();
-  delay(410);
-
-  alternate_squares_sides_and_tops(leds, 410, CRGB::SkyBlue, CRGB::ForestGreen);
+  }
+  alternate_squares_sides_and_tops(leds, 420, CRGB::SkyBlue, CRGB::ForestGreen);
   allLights.turnOff();
-  delay(410);
-  strobe_squares();
-  allLights.turnOff();
-  delay(410);
-  second_of_combo();
-  delay(410);
-  allLights.turnOff();
-  strobe_squares();
-  delay(410);
-  allLights.turnOff();
-  top_to_bottom(leds, 410, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(410);
-  allLights.turnOff();
-  strobe_squares();
-  delay(410);
-  allLights.turnOff();
-  alternate_squares_sides_and_tops(leds, 410, CRGB::SkyBlue, CRGB::ForestGreen);
-  allLights.turnOff();
-  delay(410);
-  strobe_squares();
-  allLights.turnOff();
-  delay(410);
-  second_of_combo();
-  delay(410);
-  allLights.turnOff();
-  strobe_squares();
-  delay(410);
-  allLights.turnOff();
-  top_to_bottom(leds, 410, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(410);
-  allLights.turnOff();
-  strobe_squares();
-  delay(410);
-  allLights.turnOff();
+  squares_top_bottom_dash_left_right(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
+  squares_top_bottom_dash_left_right(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
+  FastLED.clear();
 
   // beem beem, beem beem, beem beem boo boo 
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_top_bottom_dash_left_right(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(500);
+  for ( int i = 0; i < 3; i++ ){
+    squares_left_right_dash_up_down(leds, 2, CRGB::SkyBlue, CRGB::SkyBlue);
+    delay(120);
+    squares_top_bottom_dash_left_right(leds, 2, CRGB::SkyBlue, CRGB::SkyBlue);
+    delay(100);
+    trace_square_with_delays(leds, 7, 4, 145, CRGB::SkyBlue, CRGB::SkyBlue);
+    delay(25);
+  }
+  FastLED.clear();
 
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_top_bottom_dash_left_right(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(500);
+  delay(480);
+  switch_between_sets_of_lights(leds, 800, CRGB::Cyan);
+  allLights.turnOff();
+  delay(140);
 
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_top_bottom_dash_left_right(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(100);
-  squares_left_right_dash_up_down(leds, 3, CRGB::SkyBlue, CRGB::SkyBlue);
-  delay(500);
+  for (int i = 0; i <= 2; i++){
+    delay(406);
+    switch_between_sets_of_lights(leds, 410, CRGB::Gold);
+    delay(406);
+    allLights.turnOff();
+    switch_between_sets_of_lights(leds, 410, CRGB::DeepPink);
+    allLights.turnOff();
+  }
 
-  switch_between_sets_of_lights(leds, 600, CRGB::Cyan);
+  delay(700);
+  switch_between_sets_of_lights(leds, 790, CRGB::Cyan);
+  delay(660);
+  FastLED.clear();
+
+  for (int i = 0; i < 3; i++){
+    up_down_trace(leds, 5, 5, CRGB::SkyBlue, CRGB::SkyBlue);
+    delay(320);
+    FastLED.clear();
+    left_right_trace(leds, 5, 5, CRGB::SkyBlue, CRGB::SkyBlue);
+    FastLED.clear();
+    delay(320);
+    up_down_trace(leds, 5, 7, CRGB::SkyBlue, CRGB::SkyBlue);
+    FastLED.clear();
+    delay(50);
+    left_right_trace(leds, 5, 7, CRGB::SkyBlue, CRGB::SkyBlue);
+    FastLED.clear();
+    delay(320);
+  }
+
+  delay(20);
+  set_both_squares_to_color(leds, CRGB::SkyBlue);
+  FastLED.show();
+  delay(815);
+  allLights.setToColor(CRGB::SkyBlue);
+  FastLED.show();
+  allLights.fadeToBlack(36);
+
+  blink_all_lights_with_ending(leds,CRGB::White, 200);
+  delay(200);
+  blink_all_lights_with_ending(leds, CRGB::Blue, 200);
+  delay(200);
+  FastLED.clear();
+  FastLED.show();
+  blink_all_lights_with_ending(leds, CRGB::Gold, 200);
+  delay(200);
+  blink_all_lights(leds, CRGB::Cyan, 200);
+  allLights.fadeToBlack(1);
+  FastLED.show();
+
+  delay(320);
+  for (int i = 0; i <= 75; i++){
+    FastLED.clear();
+    square_side_cycle(leds, 35, CRGB::Cyan, CRGB::Red, CRGB::Green, CRGB::Fuchsia);
+    delay(35);
+  }
+
+  allLights.fadeToBlack(25);
+
+  for (int i = 0; i < 6; i++){
+    trace_upwards(leds, 5, 20, CRGB::Cyan);
+    trace_upwards(leds, 5, 20, CRGB::Gold);
+    trace_upwards(leds, 5, 20, CRGB::Fuchsia);
+  }
+
   delay(300);
 
   for (int i = 0; i <= 3; i++){
-    delay(200);
-    switch_between_sets_of_lights(leds, 600, CRGB::Gold);
-    delay(200);
-    switch_between_sets_of_lights(leds, 600, CRGB::ForestGreen);
-    delay(200);
-    switch_between_sets_of_lights(leds, 600, CRGB::DeepPink);
-    delay(200);
-    switch_between_sets_of_lights(leds, 600, CRGB::White);
+    FastLED.clear();
+    sides_to_middle(leds, 300, CRGB::SkyBlue, CRGB::SkyBlue, CRGB::SkyBlue);
+    FastLED.clear();
+    delay(500);
+    FastLED.clear();
+    set_both_squares_to_color(leds, CRGB::SkyBlue);
+    FastLED.show();
+    delay(500);
+    FastLED.clear();
+    alternate_squares_sides_and_tops(leds, 300, CRGB::SkyBlue, CRGB::SkyBlue);
+    delay(500);
+    FastLED.clear();
+    top_to_bottom(leds, 300, CRGB::SkyBlue, CRGB::SkyBlue);
+    delay(500);
+    FastLED.clear();
+    alternate_squares_sides_and_tops(leds, 300, CRGB::SkyBlue, CRGB::SkyBlue);
+    FastLED.show();
   }
-
-  // TODO: Fix sequence_one to be used here
-
 }
 
 void second_of_combo(){
@@ -146,7 +174,7 @@ void second_of_combo(){
   LightSegment middleLeft = get_middle_left(leds);
   LightSegment middleRight = get_middle_right(leds);
   SegmentList middleSides = SegmentList(&middleLeft);
-  middleSides.add(middleRight);
+  middleSides.add(&middleRight);
 
   squareSides.setAllToColor(CRGB::SkyBlue);
   FastLED.show();
@@ -154,16 +182,27 @@ void second_of_combo(){
   squareSides.turnAllOff();
   middleSides.setAllToColor(CRGB::SkyBlue);
   FastLED.show();
-
 }
 
 void strobe_squares(){
   set_both_squares_to_color(leds, CRGB::SkyBlue);
   FastLED.show();
-  delay(205);
+  delay(180);
   set_both_squares_to_color(leds, CRGB::Black);
   FastLED.show();
-  delay(205);
+  delay(180);
   set_both_squares_to_color(leds, CRGB::ForestGreen);
+  FastLED.show();
+}
+
+void strobe_middle(){
+  LightSegment middleArch = get_middle_arch(leds);
+  middleArch.setToColor(CRGB::SkyBlue);
+  FastLED.show();
+  delay(180);
+  middleArch.turnOff();
+  FastLED.show();
+  delay(180);
+  middleArch.setToColor(CRGB::Cyan);
   FastLED.show();
 }
